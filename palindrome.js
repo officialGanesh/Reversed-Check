4730a1b1b33f4a8536face18644d705b92dfd14b
Ganesh <ganuofficial2795@gmail.com>
2021-10-07 20:26:58 +0530

    Checked for palindrome

--- palindrome.js
+++ palindrome.js
@@ -8,7 +8,7 @@ class UI{
         let title = document.querySelector("#title");
         let div = document.createElement("div");
         div.classList = `alert alert-info`;
-        div.innerHTML = `<h5>${msg}</h5>`;
+        div.innerHTML = `<h5>Something Went Wrong: ${msg}</h5>`;
         cont.insertBefore(div,title);
         
         setTimeout(()=>{document.querySelector(".alert").remove()},1500);
@@ -19,10 +19,11 @@ class UI{
         let cont = document.querySelector(".container");  
         let div = document.createElement("div");
         div.classList = `alert-primary`;
+        div.id = "false"
         div.innerHTML = `<h3>Not a Palindrome 😢</h3>`
         div.style.cssText = `width:43rem; height:20rem; padding-top:7rem; border-radius:10px;`;
-        console.log(div);
         cont.append(div);
+        setTimeout(()=>{document.querySelector("#false").remove()},1200);
     };
 
     static trueOutput(){
@@ -30,12 +31,28 @@ class UI{
         let cont = document.querySelector(".container");  
         let div = document.createElement("div");
         div.classList = `alert-primary`;
+        div.id = "true"
         div.innerHTML = `<h3> Palindrome ✔️</h3>`
         div.style.cssText = `width:43rem; height:20rem; padding-top:7rem; border-radius:10px;`;
-        console.log(div);
         cont.append(div);
+        setTimeout(()=>{document.querySelector("#true").remove();},1200);
+        
     };
 
 };
 
+document.querySelector("#check").addEventListener('click',(e)=>{
 
+    let userText = document.querySelector("#text");
+    
+    if(userText.value==='' || userText.value===null){
+        UI.showAlert("No Input Given.")
+    }else{
+
+        let reversedUserText = userText.value.split("").reverse().join("");
+        // Check for palindrome using ternary operator
+        (userText.value===reversedUserText) ? UI.trueOutput() : UI.falseOutput()
+    }
+    
+    userText.value = ""
+});
